4788246c895f7c12bee54eb055bd0a114f88e3ad
zzonsang <zzonsang2@gmail.com>
2012-02-29 13:45:57 +0900

    [+] testcase

--- django_bookmarks/django_bookmarks/bookmarks/tests.py
+++ django_bookmarks/django_bookmarks/bookmarks/tests.py
@@ -6,11 +6,39 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
 
+class ViewTest(TestCase):
+    fixtures = ['test_data.json']
+    def setUp(self):
+        self.client = Client()
+        
+    def test_register_page(self):
+        data = {
+                'username' : 'tester',
+                'email' : 'tester@example.com',
+                'password1' : '123',
+                'password2' : '123',
+                }
+        response = self.client.post('/register/', data)
+        self.assertEqual(response.status_code, 302)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_bookmark_save(self):
+#        self.client.login('/save/', {'username':'test_user', 'password':'pass123'})
+        response = self.client.login(username='kobe', password='123')
+        self.assertTrue(response)
+
+        data = { 
+                'url' : 'http://www.naver.com/',
+                'title' : 'Test URL',
+                'tags' : 'test-tag'
+                }
+        response = self.client.post('/save/', data)
+        self.assertEqual(response.status_code, 302)
+        
+        response = self.client.get('/user/kobe/')
+        self.assertTrue('http://www.naver.com/' in response.content)
+        self.assertTrue('Test URL' in response.content)
+        self.assertTrue('test-tag' in response.content)
+        
+        
--- django_bookmarks/django_bookmarks/settings.py
+++ django_bookmarks/django_bookmarks/settings.py
@@ -23,7 +23,18 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+#        'TEST_DEPENDENCIES': ['slave'],
+    },
+#    'slave': {
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+##        'NAME': '/Users/kobe/dev/workspaces/python/django_bookmarks/sqlite.db',                      # Or path to database file if using sqlite3.
+#        'NAME': os.path.join(PROJECT_HOME,'bookmarks_slave.db'),
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#        'TEST_MIRROR': 'default'
+#    }
 }
 
 SITE_HOST = '127.0.0.1:8000'
