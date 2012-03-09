aa553fad45b659922ef90964b2989d4bb624d923
zzonsang <zzonsang2@gmail.com>
2012-03-09 17:02:00 +0900

    customizing admin url page

--- django_bookmarks/django_bookmarks/bookmarks/admin_model.py
+++ django_bookmarks/django_bookmarks/bookmarks/admin_model.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.conf.urls.defaults import patterns
+from django.http import HttpResponse
 
 class AdminBookmark(admin.ModelAdmin):
     list_display = ("title", "link", "user", )
     list_filter = ("user", )
     ordering = ("title", )
     search_fields = ("title", )
+    
+    def get_urls(self):
+        urls = super(AdminBookmark, self).get_urls()
+        my_urls = patterns('',
+                           (r'^my_view/$', self.admin_site.admin_view(self.my_view))
+                           )
+        return my_urls + urls
+    
+    def my_view(self, request):
+        return HttpResponse("Hello Custom Page")
     
