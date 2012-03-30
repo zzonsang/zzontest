90035f90601094f683595cf625e0c4569cc7865a
zzonsang <zzonsang2@gmail.com>
2012-03-30 16:07:49 +0900

    add search(report, plan)

--- weekly_report_2/vdi_report/admin.py
+++ weekly_report_2/vdi_report/admin.py
@@ -1,9 +1,9 @@
 # -*- encoding: UTF-8 -*-
 from django.contrib import admin
-from vdi_report.models import Report, CustomFeed
 from django.contrib.auth.models import User
-from vdi_report.admin_action import view_contents, export_excel_contents
 from django.contrib.sites.models import Site
+from vdi_report.admin_action import export_excel_contents
+from vdi_report.models import Report, CustomFeed
 import logging
 
 logger = logging.getLogger('weekly_report')
@@ -16,10 +16,11 @@ def customizing_username(obj):
 Report 모델에 대한 Admin 모델 
 '''
 class ReportAdmin(admin.ModelAdmin):
-    # 'name'으로 처리한 것은 정렬이 안된다. 한글이라 안되는 건 아닌 듯하구??
+    # 'name'으로 처리한 것은 정렬이 안된다.
     list_display = ( 'name', "date", "content", "content_next", )
     list_display_links = ("name", "content", "content_next", "date")
     list_filter = ("user__first_name", "date")
+    search_fields = ['date', 'content', 'content_next']
     
     def queryset(self, request):
         qs = super(ReportAdmin, self).queryset(request)
@@ -32,6 +33,7 @@ class ReportAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "user":
             kwargs["queryset"] = User.objects.filter(username=request.user)
+            
         return super(ReportAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
    
     def get_actions(self, request):
@@ -61,6 +63,6 @@ admin.site.unregister(Site)
 '''
 Action 등록 
 '''
-admin.site.add_action(view_contents, 'View_Contents')
+#admin.site.add_action(view_contents, 'View_Contents')
 admin.site.add_action(export_excel_contents, 'Export_Excel_Contents')
 
